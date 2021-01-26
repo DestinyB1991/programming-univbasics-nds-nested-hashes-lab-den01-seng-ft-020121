e5382a5010d43b5238d0783f15c7bed3883fb4e5
Destiny Boling <bolingdestinyr@gmail.com>
2021-01-26 00:28:36 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -24,6 +24,12 @@ def literal_aoh
 end
 
 def aoh_lookup(aoh, row, key)
+  results =  [
+{ name:  "Don Gately", occupation: "Live-in Staff" },
+{ name:  "Joelle van Dyne", occupation: "Radio Personality" },
+{ name:  "Pat Monteseian", occupation: "Staff" },
+{ name:  "Kate Gompert", occupation: "None" },
+{ name:  "Bruce Green", occupation: "Fan of Mildred" }]
   results[0][occupation]
 end
 
